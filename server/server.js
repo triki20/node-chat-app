93d1173ffec770165fd3e7fe0d28a7144b9c2cac
yahalom <triki20@gmail.com>
2019-02-17 17:06:47 +0200

    lowerCase name room and dup user names

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const {generateMessage, generateLocationMessage} = require('./utils/message');
-const {isRealString} = require('./utils/validation');
+const {isRealString,uniqueUserName} = require('./utils/validation');
 const {Users} = require('./utils/users');
 
 const publicPath = path.join(__dirname, '../public');
@@ -13,9 +13,9 @@ var app = express();
 var server = http.createServer(app)
 var io = socketIO(server);
 var users = new Users();
+var roomArray = [];
 
 app.use(express.static(publicPath));
-
 
 io.on('connection', (socket) => {
     console.log('New user connected');
@@ -23,14 +23,21 @@ io.on('connection', (socket) => {
     socket.on('join', (params, callback) => {
         if(!isRealString(params.name) || !isRealString(params.room)){
            return callback('Name and Room name are required.');
+        }else{
+            if(uniqueUserName(params.room, params.name, users.users)){
+                return callback('User name has been taken.');
+            }
         }
-        socket.join(params.room);
-        users.removeUser(socket.id);
-        users.addUser(socket.id, params.name, params.room);
 
-        io.to(params.room).emit('updateUserList', users.getUserList(params.room));
-        socket.emit('newMessage',generateMessage('Admin',`Hello ${params.name} Wellcom to ${params.room} chat`));
-        socket.broadcast.to(params.room).emit('newMessage',generateMessage('Admin',`${params.name} has joined.`));
+        var lowerCase = params.room.toLowerCase();
+
+        socket.join(lowerCase);
+        users.removeUser(socket.id);
+        users.addUser(socket.id, params.name, lowerCase);
+
+        io.to(lowerCase).emit('updateUserList', users.getUserList(lowerCase));
+        socket.emit('newMessage',generateMessage('Admin',`Hello ${params.name} Wellcom to ${lowerCase} chat`));
+        socket.broadcast.to(lowerCase).emit('newMessage',generateMessage('Admin',`${params.name} has joined.`));
 
         callback();
     });
--- server/utils/validation.js
+++ server/utils/validation.js
@@ -2,4 +2,9 @@ var isRealString = (str) => {
     return typeof str === 'string' && str.trim().length > 0;
 };
 
-module.exports = {isRealString};
+var uniqueUserName = (room,name, users) => {
+  var sameUser = users.filter((user) => user.room === room && user.name === name).length;
+  return sameUser !== 0;
+}
+
+module.exports = {isRealString,uniqueUserName};
